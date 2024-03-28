1cb256e30ad63f9f119af96cd8e2757441701e03
hallstrom91 <simonhallstrom@live.se>
2024-03-28 11:44:49 +0100

    update

---
+++ src/components/AddAuction.jsx
@@ -0,0 +1,10 @@
+export default function AddAuction() {
+  return (
+    <>
+      <h1>
+        Container / Card för att lägga till auktions-objekt i /pages/Auctions.
+        Skapa värden med props. Behövs inga api-anrop här
+      </h1>
+    </>
+  );
+}
